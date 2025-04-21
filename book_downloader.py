a82434ec4a91cecfa14ec80e76e4db34f167b7d1
Daniel <danielbnhz@gmail.com>
2025-04-21 02:30:35 -0700

    made basic features to print results in cluttered manner so far. Needs refactoring

--- book_downloader.py
+++ book_downloader.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome() 
 
@@ -11,7 +12,12 @@ search_query = input("What do you wish to search for? Input simple string:\n")
 search_box.send_keys(search_query)  
 search_box.send_keys(Keys.RETURN) 
 
+
+table = driver.find_elements("xpath","/html/body/table[3]/tbody")
+for row in table:
+    print(row.text)
+
+
 input("Press any button to continue")
 
-# Step 4: Close the browser
 driver.quit()
